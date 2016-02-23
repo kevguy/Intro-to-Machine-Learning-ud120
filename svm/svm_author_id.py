38944cdfe0d38e85a1b3f8c836c0fe75c8d750fb
kevguy <kevlai@uw.edu>
2016-02-23 16:23:09 +0800

    Use full training dataset for SVM Rdb kernel

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,8 +24,8 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-features_train = features_train[:len(features_train)/100] 
-labels_train = labels_train[:len(labels_train)/100] 
+### features_train = features_train[:len(features_train)/100] 
+### labels_train = labels_train[:len(labels_train)/100] 
 
 from sklearn.svm import SVC
 clf = SVC(kernel = 'rbf', C=10000)
