5bf75ab524a6cd15d4dd1e1400bf50a561853590
kevguy <kevlai@uw.edu>
2016-02-23 16:14:51 +0800

    Add code to slice down svm training data set

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,6 +24,9 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100] 
+
 from sklearn.svm import SVC
 clf = SVC(kernel = 'linear')
 print 'Training begins'
