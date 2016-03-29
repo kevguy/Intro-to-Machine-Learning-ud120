a5e3afbce3ad5c826237e9ac3aad751c8ff8458e
kevguy <kevlai@uw.edu>
2016-03-29 08:34:24 +0800

    feat: add code to deploying k-mean clustering

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -64,8 +64,10 @@ plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
-
-
+from sklearn.cluster import KMeans
+clf = KMeans(n_clusters = 2)
+clf.fit(data)
+pred = clf.predict(data)
 
 
 ### rename the "name" parameter when you change the number of features
