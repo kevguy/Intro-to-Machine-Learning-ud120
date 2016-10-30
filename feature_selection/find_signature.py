f76b8635d9770d97d1233da51203d4ff4e21b7fd
kevguy <kevlai22@uw.edu>
2016-10-30 16:26:48 +0800

    feat: add code to find accuracy of decision tree in feature selection

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -8,8 +8,8 @@ numpy.random.seed(42)
 ### The words (features) and authors (labels), already largely processed.
 ### These files should have been created from the previous (Lesson 10)
 ### mini-project.
-words_file = "../text_learning/your_word_data.pkl" 
-authors_file = "../text_learning/your_email_authors.pkl"
+words_file = "../feature_selection/word_data_overfit.pkl" 
+authors_file = "../feature_selection/email_authors_overfit.pkl"
 word_data = pickle.load( open(words_file, "r"))
 authors = pickle.load( open(authors_file, "r") )
 
@@ -38,6 +38,10 @@ labels_train   = labels_train[:150]
 
 
 ### your code goes here
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+print clf.score(features_test, labels_test)
 
 
 
