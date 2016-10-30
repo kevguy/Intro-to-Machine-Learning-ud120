7166ec43c340b7d1b0ad269ff705e3e688e2dcbd
kevguy <kevlai22@uw.edu>
2016-10-31 01:15:51 +0800

    feat: add code for 'Accuracy for your overfitting decision tree'

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -8,8 +8,8 @@ numpy.random.seed(42)
 ### The words (features) and authors (labels), already largely processed.
 ### These files should have been created from the previous (Lesson 10)
 ### mini-project.
-words_file = "../feature_selection/word_data_overfit.pkl" 
-authors_file = "../feature_selection/email_authors_overfit.pkl"
+words_file = "../text_learning/your_word_data.pkl" 
+authors_file = "../text_learning/your_email_authors.pkl"
 word_data = pickle.load( open(words_file, "r"))
 authors = pickle.load( open(authors_file, "r") )
 
@@ -44,11 +44,11 @@ clf = clf.fit(features_train, labels_train)
 print clf.score(features_test, labels_test)
 
 feature_importances = clf.feature_importances_
-result = filter(lambda x: x > 0.15, feature_importances)
+result = filter(lambda x: x > 0.2, feature_importances)
 result = max(feature_importances)
 print result
 print numpy.where(feature_importances==result)
-print feature_importances[33604]
+print feature_importances[33614]
 
 import sklearn.feature_extraction.text.TfidfVectorizer 
 
